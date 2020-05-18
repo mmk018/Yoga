d0b47465d0eb4be422ccb8209335326acd8ba0ca
Maks <mn27305@gmail.com>
2020-05-18 21:48:14 +0200

    local storage

--- js/script.js
+++ js/script.js
@@ -356,3 +356,55 @@ place.addEventListener('change', function() {
         totalValue.innerHTML = a * this.options[this.selectedIndex].value;
     }
 });    
+
+
+///Local storage
+
+localStorage.setItem('number',1);//
+
+console.log(localStorage.getItem('number'));
+
+localStorage.removeItem('number');
+
+localStorage.clear();
+
+window.addEventListener('DOMContentLoaded', function() {
+    let checkbox = document.getElementById('rememberMe'),
+    change = document.getElementById('change'),
+    form = document.getElementsByTagName('form')[0];
+
+
+    if(localStorage.getItem('isChecked') === 'true') {
+        checkbox.checked = true;
+    }
+
+    if(localStorage.getItem('bg') === 'changed') {
+        form.style.backgroundColor = '#333';
+    }
+
+    checkbox.addEventListener('click', function() {
+        localStorage.setItem('isChecked',true);//esli input checked
+    })
+    change.addEventListener('click', function() {
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = '#333';
+    })
+
+    let persone = {
+        name: 'Alex',
+        age: 25,
+        tech: ['mobile', '_notebook_']
+    }
+
+    let serializedPersone = JSON.stringify(persone);
+
+    localStorage.setItem('Alex', serializedPersone);
+    console.log(JSON.parse(localStorage.getItem('Alex')));
+    
+
+})
+
+
+
+
+
