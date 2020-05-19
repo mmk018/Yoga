3c2900b42465d192599a43e4dcbaf10a094b6b08
Maks <mn27305@gmail.com>
2020-05-20 00:32:45 +0200

    webpack with babel and ES6 Promise and foreach+FormData  convert

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
+require('es6-promise').polyfill();
+require('formdata-polyfill');
+require('nodelist-foreach-polyfill');
+
+
+
 window.addEventListener('DOMContentLoaded' , function () {
 
     'use strict';
